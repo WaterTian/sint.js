a3f02ffbad1d46c8da727d99e9b4ffe1a8969a41
tianyuan <tianyuan5@vipkid.com.cn>
2018-11-05 22:10:24 +0800

    export  pixi

--- src/sint.js
+++ src/sint.js
@@ -10,17 +10,22 @@ import TextClip from './core/TextClip';
 //	TOOLS
 import TweenMax from "gsap";
 
+import {Container,Sprite,filters} from "pixi.js";
+
 export {
 	Game,
 	CONST,
 	Unit,
 	Magic,
 	TweenMax,
+	Container,
+	filters,
+	Sprite,
 	SpineClip,
 	SpriteClip,
 	AnimatedClip,
 	TextClip,
 };
 
-
-global.SINT = Object.assign(global.PIXI, exports);
+global.SINT = exports;
+// global.SINT = Object.assign(global.PIXI, exports);
