b2378ecd8a36f38e2ddf88d8a35303dfdc8f6eb7
WaterTian <changewater@qq.com>
2018-12-10 19:25:02 +0800

    complete AnimatedClip

---
+++ src/game/index.js
@@ -0,0 +1,7 @@
+/**
+ * @namespace SINT.game
+ */
+export { default as Game } from './Game';
+export { default as SpriteClip } from './SpriteClip';
+export { default as TextClip } from './TextClip';
+export { default as AnimatedClip } from './AnimatedClip';
--- src/sint.js
+++ src/sint.js
@@ -1,48 +1,44 @@
+import * as Unit from './Unit';
+import * as Magic from './magic';
 
-import Game from './game/Game';
-// import * as Unit from './Unit';
-// import * as Magic from './magic/';
-// // // import SpineClip from './core/SpineClip';
-// import AnimatedClip from './game/AnimatedClip';
-import SpriteClip from './game/SpriteClip';
-import TextClip from './game/TextClip';
 import VideoDom from './VideoDom';
 
-// import { TweenLite } from "gsap/TweenLite";
-// import { EasePack } from "gsap/EasePack"; 
-// const Tween = TweenLite;
 import Tween from './Tween';
-
-
-import { AudioManager } from './audio';
-
-export * from './core';
 
 import * as interaction from './interaction';
 import * as loaders from './loaders';
 
 // handle mixins now, after all code has been added, including deprecation
-import { utils } from './core';
+import {
+	utils
+} from './core';
 utils.mixins.performMixins();
 
 
+
+export * from './core';
+
 export {
 	Game,
-	// Unit,
-	// Magic,
-	// // // SpineClip,
 	SpriteClip,
-	// AnimatedClip,
 	TextClip,
+	AnimatedClip
+}
+from './game';
+
+export {
+	AudioManager
+} from './audio';
+
+export {
+	Unit,
+	Magic,
 	VideoDom,
-	
 	Tween,
-	AudioManager,
 	interaction,
 	loaders,
 };
 
 
 // Always export SINT globally.
-global.SINT = exports; // eslint-disable-line
-
+global.SINT = exports;
