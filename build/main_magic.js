ed9e0c988853fef36ba2c03e809a691de978d70d
WaterTian <changewater@qq.com>
2018-12-21 21:02:03 +0800

    save

--- build/main_magic.js
+++ build/main_magic.js
@@ -1,4 +1,3 @@
-
 const config = {
 	domElement: document.querySelector('#webglContainer'), // 画布容器
 	initWidth: 500,
@@ -19,16 +18,26 @@ var filter = new SINT.magic.HolesFilter();
 bg.filters = [filter];
 
 
-if (window.DeviceOrientationEvent) window.addEventListener('deviceorientation', onOrientation)
+bg.interactive = true;
+bg.hitArea = new SINT.Rectangle(0, 0, game.initWidth, game.initHeight);
+bg.on('pointermove', function(event) {
+	var p = event.data.global.clone();
 
-function onOrientation(event) {
-	// var alpha = event.alpha ? SINT.Unit.degToRad(event.alpha) : 0 // Z
-	var beta = event.beta ? SINT.Unit.degToRad(event.beta) : 0 // X'
-	var gamma = event.gamma ? SINT.Unit.degToRad(event.gamma) : 0 // Y''
+	filter.offset[0] += (p.x / game.initWidth - filter.offset[0]) * 0.1;
+	filter.offset[1] += (p.y / game.initHeight - filter.offset[1]) * 0.1;
+})
 
-	var _rx = beta;
-	var _ry = gamma;
-	
-	filter.offset[0]+=(_rx-filter.offset[0])*0.1;
-	filter.offset[1]+=(_ry-filter.offset[1])*0.1;
-}
+
+// if (window.DeviceOrientationEvent) window.addEventListener('deviceorientation', onOrientation)
+
+// function onOrientation(event) {
+// 	// var alpha = event.alpha ? SINT.Unit.degToRad(event.alpha) : 0 // Z
+// 	var beta = event.beta ? SINT.Unit.degToRad(event.beta) : 0 // X'
+// 	var gamma = event.gamma ? SINT.Unit.degToRad(event.gamma) : 0 // Y''
+
+// 	var _rx = beta;
+// 	var _ry = gamma;
+
+// 	filter.offset[0]+=(_rx-filter.offset[0])*0.1;
+// 	filter.offset[1]+=(_ry-filter.offset[1])*0.1;
+// }
